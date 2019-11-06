057be5242046a24c5a6c5d521e290b42886ee109
limyongtae <lyt0324@ibizsoftware.net>
2019-11-06 09:06:31 +0900

    add out of memory test project

---
+++ memory-out/src/main/java/tuning/project/TuningApplication.java
@@ -0,0 +1,11 @@
+package tuning.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuningApplication {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(TuningApplication.class, args);
+	}
+}
